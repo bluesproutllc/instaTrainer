ec8a658280c4301b4b83d1cf72d61b24873b676c
Linh Tran <linhtanl51@gmail.com>
2021-02-08 23:08:03 -0600

    added controller for ssid cookie and create user for trainer

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,4 +1,42 @@
+const db = require('../model/dbModel');
+const bcrypt = require('bcrypt');
+const saltRounds = 15;
+
 const authControllers = {};
 
-// setSSIDCookie - store 
+// setSSIDCookie - store the user id from database in cookie
+authControllers.setSSIDCookie = (req, res, next) => {
+    // set cookie with key 'ssid' to value user's id 
+    res.cookie('ssid', res.locals.id, {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+    })
+    return next()
+}
+// createUser for sign-up 
+authControllers.createUsers = (req, res, next) => {
+    // check if user checks 'trainer' or 'client' box
+    const {userType} = req.body;
+    // if user is a trainer, extract fullname, username, password
+    if (userType === 'trainer') {
+        const {trainer_name, username, password} = req.body;
+        const param = [username.toLowerCase()];
+        db.query(`SELECT * FROM trainers WHERE (username = $1);`, param).then(data => {
+            if (data.rows.length) {
+                res.locals.status = 'trainer exists';
+                return next();
+            } else {
+                bcrypt.hash(password, saltRounds, (err, hash) => {
+                    const hashedPassword = hash;
+                    const values = [username.toLowerCase(), hashedPassword, trainer_name];
+                    db.query(`INSERT INTO trainers (username, password, trainer_name) VALUES ($1, $2, $3);`, values)
+                    .then((data) => next())
+                    .catch((err) => next({err}))
+                })
+            }
+        })
+    } 
+
+}
 module.exports = authControllers;
