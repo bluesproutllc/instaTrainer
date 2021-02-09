48821732df72a1cda711fdb74374d9360bec695b
DennisLpz <admin@DLo-MacBook-Pro.lan>
2021-02-08 16:00:38 -0800

    Adding BoilerPlate

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, '../src/index.html'));
+});
+
+
+app.use((req, res) => res.sendStatus(404)); //catch-all route handler--for unknown routes
+
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}... ya bish`); })
