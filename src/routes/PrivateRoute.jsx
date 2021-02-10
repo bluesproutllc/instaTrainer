15fa94ae79ab80cfe66e55c62e543a589e3c9bd2
Matt Jiang <mattljiang@gmail.com>
2021-02-10 00:49:51 -0800

    use isLoggedIn function in PrivateRoute

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-  const userType = 'client';
+  const isLoggedIn = () => {
+    return true;
+    return false;
+  }
   return (
 
       // Show the component only when the user is logged in
       // Otherwise, redirect the user to /signin page
       <Route {...rest} render={props => (
-          (userType === 'client')?
+          isLoggedIn() ?
               <Component {...props} />
           : <Redirect to="/signup" />
       )} />
