a534c6e65893757b8e74cb20803ab567af4105d6
DennisLpz <admin@DLo-MacBook-Pro.lan>
2021-02-10 12:17:52 -0800

    Added Fetch request to client view

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -2,6 +2,27 @@ import React, { Fragment, useState, useEffect } from 'react';
 import ExercisesCard from './ExercisesCard.jsx';
 function Client() {
   const [clientInfo, setclientInfo] = useState();
+  const [getExercises, setgetExercises] = useState([]);
+  //will remove use effect once enpoint has been connected with backend
+  useEffect(() => {
+    //get client id from cookie
+    fetch(`/api/clients/dashboard/`)
+      .then((res) => res.json())
+      .then((response) => {
+        const exerciseCards = response.map((exercise) => {
+          return (
+            <ExercisesCard
+              duration={exercise.plan_duration}
+              frequency={exercise.frequency}
+              notest={exercise.notes}
+            />
+          );
+        });
+        setgetExercises(exerciseCards);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  //tentative have loop to show cards - will wait on backend endpoint connection later
   const exerciseCards = [];
   for (let i = 0; i < 5; i += 1) {
     exerciseCards.push(<ExercisesCard />);
--- src/components/ExercisesCard.jsx
+++ src/components/ExercisesCard.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
-function ExercisesCard() {
+function ExercisesCard(props) {
+  //console.log(props)
   return (
     <div className='div-container'>
       <div className='client-card-container'>
@@ -12,9 +13,21 @@ function ExercisesCard() {
         </div>
         <div className='right-side-excercise-card'>
           <div className='exercise-info-container'>
-            <p className='exercise-detail'>Duration: 15min</p>
-            <p className='exercise-detail'>Frequency: 2 times/day</p>
-            <p className='exercise-detail'>
+            <div className='duration-frequency-container'>
+              <div className='duration-box'>
+                <p id='duration-min-id' className='exercise-detail'>
+                  15min
+                </p>
+                <p className='exercise-detail'>Duration</p>
+              </div>
+              <div className='frequency-box'>
+                <p id='frequency-time-id' className='exercise-detail'>
+                  2 times/day
+                </p>{' '}
+                <p className='exercise-detail'>Frequency</p>
+              </div>
+            </div>
+            <p id='notes-id' className='exercise-detail'>
               Notes: Its grind time. No days off bro!
             </p>
           </div>
