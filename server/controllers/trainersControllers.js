e655f00ffe1fe233c05d6ca327e3baef0d866907
Linh Tran <linhtanl51@gmail.com>
2021-02-09 19:43:30 -0600

    got clients based on trainer id in params

--- server/controllers/trainersControllers.js
+++ server/controllers/trainersControllers.js
@@ -6,9 +6,10 @@ trainersControllers.assignWorkout = (req, res, next) =>{
 
 }
 trainersControllers.getClients = (req, res, next) => {
-    const {ssid} = req.cookies;
-    const param = [ssid[ssid.length - 1]];
-    db.query(`SELECT * FROM clients WHERE (trainer_id = $1);`,param)
+    // const {ssid} = req.cookies;
+    const {trainerId} = req.params;
+    const value = [trainerId];
+    db.query(`SELECT * FROM clients WHERE (trainer_id = $1);`, value)
     .then(data => {
         res.locals.clients = data.rows;
         return next()
